4122c294f0b39d289d2893d9493535d69a22cece
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-12-01 00:00:16 +0300

    Add project files.

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Faturalar.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Faturalar.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +10,22 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
     public class Faturalar
     {
         public int Faturaid { get; set; }
+        [Column(TypeName = "Char")]
+        [StringLength(1)]
         public string FaturaSeriNo { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(6)]
         public string FaturaSıraNo { get; set; }
         public DateTime Tarih { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(60)]
         public string VergiDairesi { get; set; }
         public DateTime Saat {  get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
         public string TeslimEden { get;set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
         public string TeslimAlan { get;set; }
         public ICollection<FaturaKalem> FaturaKalems { get; set; }
     }
--- MvcOnlineTicariOtomasyon/Models/Siniflar/Kategori.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Kategori.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,8 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
     {
         [Key]
         public int Kategoriid { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
         public string KategoriAd { get; set; }
         //Kategoride birden fazla ürün yer alabilir
         public ICollection<Urun> Uruns { get; set; }
