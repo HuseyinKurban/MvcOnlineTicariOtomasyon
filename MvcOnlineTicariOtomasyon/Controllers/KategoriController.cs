0a145a1b2a3d2cb1d61424cc52d3091bd699dd7d
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-12-02 18:43:21 +0300

    Add project files.

--- MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +11,48 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
+
         public ActionResult Index()
         {
+            var values = c.Kategoris.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = c.Kategoris.Find(id);
+            c.Kategoris.Remove(ktg);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori=c.Kategoris.Find(id);
+            return View("KategoriGetir",kategori);
+        }
+
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var ktg=c.Kategoris.Find(k.Kategoriid);
+            ktg.KategoriAd=k.KategoriAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -0,0 +1,21 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+
+        Context c=new Context();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
